b0b53d0794002d11d90335611e47dde96e2d9b59
Kosiv92 <kosiv92@gmail.com>
2023-10-13 00:29:06 +0800

    Implement add role method

--- OnlineShop.Web/Controllers/UserController.cs
+++ OnlineShop.Web/Controllers/UserController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShop.DbContext;
 
 namespace OnlineShop.Web.Controllers
 {
     [Authorize(Roles ="Admin")]
     public class UserController : Controller
     {
+        private readonly ILogger<UserController> _logger;
+        
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly UserManager<ApplicationIdentityUser> _userManager;
+
+        public UserController(ILogger<UserController> logger, 
+            RoleManager<IdentityRole> roleManager, 
+            UserManager<ApplicationIdentityUser> userManager)
+        {
+            _logger = logger;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -13,8 +30,21 @@ namespace OnlineShop.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddNewRole(string RoleName) 
+        public async Task<IActionResult> AddNewRole(string roleName) 
         {
+            roleName = roleName.Trim();
+
+            if (!string.IsNullOrEmpty(roleName))
+            {
+                if (!await _roleManager.RoleExistsAsync(roleName))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole
+                    {
+                        Name = roleName,
+                        NormalizedName = roleName
+                    });
+                }
+            }  
             return RedirectToAction("Index");
         }
     }
